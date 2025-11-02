5f5258c1a2d1e2a1410baab4e21f4131230b4d10
Foxyrobo <proxyrobo23@gmail.com>
2025-11-02 14:46:11 -0800

    Add object pooling
    Pipes that reach the end of the screen are put back to the right of the screen to be used again. Extra pipes only spawn in when it's necessary. This is to stop constant pipe deletion and creation.

--- Scripts/pipe_movement.gd
+++ Scripts/pipe_movement.gd
@@ -1,8 +1,22 @@
 extends Node2D
 
 
-const speed: float = -400;
+const speed: float = -400
+const respawn_x_pos: float = -700 # When the pipes go past this point, go back into pipe queue
+
+signal respawn_pipe(Node2D)
+
+var move_pipe: bool = true
 
 func _physics_process(delta: float) -> void:
-	# Move pipe
-	position.x += speed * delta;
+	if move_pipe:
+		# Move pipe
+		position.x += speed * delta;
+		
+		# Check if this is past respawn point
+		if global_position.x <= respawn_x_pos:
+			respawn_pipe.emit($".")
+
+
+func set_move_pipe(enabled: bool) -> void:
+	move_pipe = enabled
--- Scripts/pipe_spawner.gd
+++ Scripts/pipe_spawner.gd
@@ -5,6 +5,7 @@ extends Node2D
 const PIPE_SCENE_TRIPLE = preload("res://Scenes/triple_pipe.tscn")
 
 var pipe_elevation_max: float = 270
+var free_pipes: Array[Node2D] = []
 
 @onready var timer: Timer = $Timer
 
@@ -14,12 +15,52 @@ func _ready() -> void:
 
 
 func create_pipe() -> void:
+	# Instantiate pipe
 	var new_pipe = PIPE_SCENE_TRIPLE.instantiate()
+	
+	# Set as child of this spawner
 	add_child(new_pipe)
 	
-	# Set y position randomly
-	new_pipe.position.y = randf_range(-pipe_elevation_max, pipe_elevation_max)
+	# Set pipe position and stats
+	initialize_pipe(new_pipe)
+	
+	# Make pipe remove itself when it reaches end of screen
+	new_pipe.respawn_pipe.connect(remove_pipe)
+
+
+# Puts a pipe on the right side of the screen
+# This is different from create_pipe(), which instantiates a new pipe
+func spawn_pipe() -> void:
+	# Check if there is a pipe available
+	if free_pipes.size() > 0:
+		# TODO - Add random pipe from list
+		initialize_pipe(free_pipes.pop_front())
+	
+	# If there is no pipe available, instantiate a new one
+	else:
+		create_pipe()
 
 
 func _on_timer_timeout() -> void:
-	create_pipe() # TODO - Add object pooling
+	#create_pipe() # TODO - Add object pooling
+	spawn_pipe()
+
+
+func initialize_pipe(pipe_node: Node2D) -> void:
+	# Set y position randomly
+	pipe_node.position.y = randf_range(-pipe_elevation_max, pipe_elevation_max)
+	
+	# Start movement
+	pipe_node.set_move_pipe(true)
+
+
+# Removes pipe from scene and adds it to the free_pipes array
+func remove_pipe(pipe_node: Node2D) -> void:
+	# Stop pipe movement
+	pipe_node.set_move_pipe(false)
+	
+	# Set position to offscreen (where it will start moving again)
+	pipe_node.global_position.x = global_position.x
+	
+	# Add this to queue
+	free_pipes.push_back(pipe_node)
