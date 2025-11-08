e97219ad660cdaa0be49c4cd93104ed5aa968351
Foxyrobo <proxyrobo23@gmail.com>
2025-11-07 16:55:50 -0800

    Add player death
    Players disappear when they die

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,6 +6,8 @@ const JUMP_VELOCITY = -600.0
 
 @export var jumpAction: String = "jumpP1"
 
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var collision: CollisionShape2D = $CollisionShape2D
 @onready var player_explosion: Node2D = $player_explosion
 
 func _physics_process(delta: float) -> void:
@@ -23,4 +25,13 @@ func _physics_process(delta: float) -> void:
 
 # Called when player touches killzone
 func on_player_died() -> void:
+	# Start particles
 	player_explosion.get_node("CPUParticles2D").emitting = true
+	
+	# Hide player
+	sprite.process_mode = Node.PROCESS_MODE_DISABLED
+	sprite.visible = false
+	
+	# Disable collision
+	collision.process_mode = Node.PROCESS_MODE_DISABLED
+	collision.set_deferred("disabled", true)
