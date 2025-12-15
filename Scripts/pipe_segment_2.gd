18869a605ec62c2a30c288d8ff77d0cf9be12ff7
Foxyrobo <proxyrobo23@gmail.com>
2025-12-15 12:06:36 -0800

    Add proper giraffe sprites
    Add sprites for pipes of all sizes. Fix implementation of sprites

--- Scripts/pipe_segment_2.gd
+++ Scripts/pipe_segment_2.gd
@@ -1,13 +1,16 @@
 extends Sprite2D
 
+@export var scale_factor: float = 3
+
 @onready var pipe_segment: Sprite2D = $"."
 @onready var killzone: Area2D = $Killzone
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Scale sprite and killzone so they show up and collide properly
-	var scale_amount = transform.get_scale().y
-	pipe_segment.region_rect = Rect2(0, 0, pipe_segment.region_rect.size.x, pipe_segment.region_rect.size.y * scale_amount + 26) # Set sprite region amount
-	killzone.apply_scale(Vector2(1, scale_amount)) # Adjust killzone size
-	apply_scale(Vector2(1, 1 / scale_amount)) # Make sprite less stretched, while making killzone the proper size
-	pipe_segment.offset.y -= 64 * (scale_amount - 1) # Offset sprite
+	#var scale_amount = transform.get_scale().y
+	#pipe_segment.region_rect = Rect2(0, 0, pipe_segment.region_rect.size.x, pipe_segment.region_rect.size.y * scale_amount + 26) # Set sprite region amount
+	killzone.apply_scale(Vector2(1, scale_factor)) # Adjust killzone size
+	#apply_scale(Vector2(1, 1 / scale_amount)) # Make sprite less stretched, while making killzone the proper size
+	pipe_segment.offset.y -= 64 * (scale_factor - 1) - 26 # Offset sprite
+	pass
