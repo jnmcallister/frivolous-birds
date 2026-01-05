ded9b6fe8ba8e9d925d4043bbabc06c4d6a666e6
Foxyrobo <proxyrobo23@gmail.com>
2026-01-05 15:07:31 -0800

    Add button SFX
    Add SFX when hovering the mouse over a button, when pressing down on a button, and pressing up on a button with the mouse still over it.

---
+++ Scripts/basic_button.gd
@@ -0,0 +1,16 @@
+extends Button
+
+@onready var hover_sound: AudioStreamPlayer = $HoverSound
+@onready var button_down_sound: AudioStreamPlayer = $ButtonDownSound
+@onready var pressed_sound: AudioStreamPlayer = $PressedSound
+
+func _on_mouse_entered() -> void:
+	hover_sound.play()
+
+
+func _on_button_down() -> void:
+	button_down_sound.play()
+
+
+func _on_pressed() -> void:
+	pressed_sound.play()
