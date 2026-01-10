214bfe68a65ca3e51f9eabaf32513619e6f05ede
Foxyrobo <proxyrobo23@gmail.com>
2026-01-09 16:59:48 -0800

    Add slider SFX
    Play sounds when adjusting volume sliders.

--- Scripts/audio_slider.gd
+++ Scripts/audio_slider.gd
@@ -4,6 +4,8 @@ extends HSlider
 
 var bus_index: int
 
+@onready var slider_click_sound_player: AudioStreamPlayer = $SliderClickSoundPlayer
+
 func _ready() -> void:
 	# Get index of bus (bus functions use indicies instead of strings)
 	bus_index = AudioServer.get_bus_index(bus_name)
@@ -15,9 +17,22 @@ func _ready() -> void:
 	value = db_to_linear(
 		AudioServer.get_bus_volume_db(bus_index)
 	)
+	
+	# Set bus of slider click sound player
+	slider_click_sound_player.bus = bus_name
 
 func _on_value_changed(val: float) -> void:
 	# Set volume of bus to slider value
 	AudioServer.set_bus_volume_db(
 		bus_index, linear_to_db(val)
 	)
+
+
+func _on_drag_started() -> void:
+	# Start playing sounds
+	slider_click_sound_player.play()
+
+
+func _on_drag_ended(value_changed: bool) -> void:
+	# Stop playing sounds
+	slider_click_sound_player.stop()
